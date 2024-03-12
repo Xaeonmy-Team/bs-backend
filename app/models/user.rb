85675fcf406f2477b6f2a53a912698d829ecdae7
jayfr1122 <jayfranks21@gmail.com>
2024-03-12 09:59:42 -0700

    changes made to controllers, jwt code added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
   has_many :syncs
 end
